0b485258205fb1132d0951416aa6b99fd5f359a2
Jawknee530 <jallen59@mail.csuchico.edu>
2016-04-04 18:33:49 -0700

    oops

--- p2/matrixmult.cpp
+++ p2/matrixmult.cpp
@@ -7,16 +7,15 @@
 #include <time.h>
 #include <string>
 #include <math.h>
+#include <mpi.h>
+#include <vector>
 using namespace std;
 
 void Solveijk (int *A, int *B, int *C, int n);
-void Solvejik (int *A, int *B, int *C, int n);
 void Solveikj (int *A, int *B, int *C, int n);
-void Solvejki (int *A, int *B, int *C, int n);
 void Solvekij (int *A, int *B, int *C, int n);
-void Solvekji (int *A, int *B, int *C, int n);
 //figure out your range
-void getRange (int &my_i, int &my_j, int &my_k, int n, int p, int my_rank);
+//void getRange (int &my_i, int &my_j, int &my_k, int n, int p, int my_rank);
 
 
 int main(int argc, char *argv[]) {
@@ -25,6 +24,7 @@ int main(int argc, char *argv[]) {
   string flag;
   int n;
   int my_i, my_j, my_k;
+  int *A, *B, *C;
 
   //start mpi
   MPI_Init(NULL, NULL);
@@ -38,9 +38,9 @@ int main(int argc, char *argv[]) {
     cin >> flag;
     cin >> n;
 
-    int A[n*n];
-    int B[n*n];
-    int C[n*n];
+    A = (int *)malloc(n*sizeof(int));
+    B = (int *)malloc(n*sizeof(int));
+    C = (int *)malloc(n*sizeof(int));
 
     for(int i = 0; i < n*n; i++) {
       C[i] = 0;
@@ -66,25 +66,16 @@ int main(int argc, char *argv[]) {
   }
 
   //set each procs i, j, k
-  getRange(my_i, my_j, my_k, n, comm_sz);
+  //getRange(my_i, my_j, my_k, n, comm_sz);
 
   if(form == "ijk") {
     Solveijk(A, B, C, n);
   }
-  else if(form == "jik"){
-    Solvejik(A, B, C, n);
-  }
   else if(form == "ikj"){
     Solveikj(A, B, C, n);
   }
-  else if(form == "jki"){
-    Solvejki(A, B, C, n);
-  }
   else if(form == "kij"){
     Solvekij(A, B, C, n);
-  }
-  else if(form == "kji"){
-    Solvekji(A, B, C, n);
   }
   
 
@@ -107,6 +98,7 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+/*
 void getRange (int &my_i, int &my_j, int &my_k, int n, int p, int my_rank) {
   if(p < n) {
     my_i = my_rank * (n/p) % n;
@@ -124,19 +116,11 @@ void getRange (int &my_i, int &my_j, int &my_k, int n, int p, int my_rank) {
     my_k = my_rank * ((n*n*n)/p) % n;
   }
 }
+*/
 
 void Solveijk (int *A, int *B, int *C, int n) {
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < n; j++) {
-      for(int k = 0; k < n; k++) {
-        C[i*n+j] = C[i*n+j] + A[i*n+k] * B[k*n+j];
-      }
-    }
-  }
-}
-void Solvejik (int *A, int *B, int *C, int n) {
-  for(int j = 0; j < n; j++) {
-    for(int i = 0; i< n; i++) {
       for(int k = 0; k < n; k++) {
         C[i*n+j] = C[i*n+j] + A[i*n+k] * B[k*n+j];
       }
@@ -152,28 +136,10 @@ void Solveikj (int *A, int *B, int *C, int n) {
     }
   }
 }
-void Solvejki (int *A, int *B, int *C, int n) {
-  for(int j = 0; j < n; j++) {
-    for(int k = 0; k< n; k++) {
-      for(int i = 0; i < n; i++) {
-        C[i*n+j] = C[i*n+j] + A[i*n+k] * B[k*n+j];
-      }
-    }
-  }
-}
 void Solvekij (int *A, int *B, int *C, int n) {
   for(int k = 0; k < n; k++) {
     for(int i = 0; i< n; i++) {
       for(int j = 0; j < n; j++) {
-        C[i*n+j] = C[i*n+j] + A[i*n+k] * B[k*n+j];
-      }
-    }
-  }
-}
-void Solvekji (int *A, int *B, int *C, int n) {
-  for(int k = 0; k < n; k++) {
-    for(int j = 0; j< n; j++) {
-      for(int i = 0; i < n; i++) {
         C[i*n+j] = C[i*n+j] + A[i*n+k] * B[k*n+j];
       }
     }
