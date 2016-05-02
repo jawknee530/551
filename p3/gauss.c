8c2792ed7084429dafba5034a3861b4df711d9cc
Jonathan Allen <jallen59@mail.csuchico.edu>
2016-05-01 19:58:27 -0700

    cleaning up p3

--- p3/gauss.c
+++ p3/gauss.c
@@ -12,13 +12,13 @@
 #include<omp.h>
 
 void init(double*** a, double** b, double** x, double*** M, double** B, int n);
-void fill_mat(double*** a, double**b, double*** M, double** B, int n);
-void pivot(double*** a, double* b, int j, int n);
-void gauss(double*** a, double* b, int i, int n, int thread_count);
-void gauss_row(double*** a, double* b, int i, int j, int n, double t, int start
+void fill_mat(double** a, double*b, double** M, double* B, int n);
+void pivot(double** a, double* b, int j, int n);
+void gauss(double** a, double* b, int i, int n, int thread_count);
+void gauss_row(double** a, double* b, int i, int j, int n, double t, int start
               , int thread_count);
-void back_sub(double*** a, double** b, double** x, int n);
-double solve_iS(double*** M, double** B, double** x, int n);
+void back_sub(double** a, double* b, double* x, int n);
+double solve_iS(double** M, double* B, double* x, int n);
 void set_test(double*** a, double** b, int n);
 void print_mats(double*** a, double** b, int n);
 
@@ -53,28 +53,28 @@ int main(int argc, char* argv[])
 
   //initialize the matrix and array memories then fill them using drand48()
   init(&a, &b, &x, &M, &B, n);
-  fill_mat(&a, &b, &M, &B, n);
-  
+  fill_mat(a, b, M, B, n);
+
   //start the timer before the solution starts
   start = omp_get_wtime();
 
   //actual soluction: pivot, gauss, back_sub
   for(int j = 0; j < n; j++) {
-    pivot(&a, b, j, n);
-    gauss(&a, b, j, n, thread_count);
+    pivot(a, b, j, n);
+    gauss(a, b, j, n, thread_count);
   }
 
-  back_sub(&a, &b, &x, n);
+  back_sub(a, b, x, n);
 
   //stop the timer once the solution is found
   finish = omp_get_wtime();
 
   //compute the iSquared-norm value to see accuracy of solution
-  iS = solve_iS(&M, &B, &x, n);
+  iS = solve_iS(M, B, x, n);
 
   //print iSquared-norm and the time the solution took
   printf(" I2-Norm is: %.10f\n\n", iS);
-  printf(" Elapsed time = %.3f seconds\n\n", finish-start);
+  printf(" Solution time = %.4f seconds\n\n", finish-start);
 }
 
 /*
@@ -105,13 +105,13 @@ void init(double*** a, double** b, double** x, double*** M, double** B, int n) {
  * the original a.
  * Does the same thing with b and B.
  */
-void fill_mat(double*** a, double**b, double*** M, double** B, int n) {
+void fill_mat(double** a, double* b, double** M, double* B, int n) {
   srand48(time(NULL));
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < n; j++) {
-      (*a)[i][j] = (*M)[i][j] = (drand48() * 2000000 - 1000000);
+      a[i][j] = M[i][j] = (drand48() * 2000000 - 1000000);
     }
-    (*b)[i] = (*B)[i] = (drand48() * 2000000 - 1000000);
+    b[i] = B[i] = (drand48() * 2000000 - 1000000);
   }
 }
 
@@ -120,7 +120,7 @@ void fill_mat(double*** a, double**b, double*** M, double** B, int n) {
  * absolute value. If the highests is not in the top
  * row then swaps it and the top in a and b.
  */
-void pivot(double*** a, double* b, int j, int n) {
+void pivot(double** a, double* b, int j, int n) {
   double max = 0;
   int cur_row = j;
   int top_row = j;
@@ -128,16 +128,16 @@ void pivot(double*** a, double* b, int j, int n) {
   double tmp_b;
 
   for(int i = j; i < n; i++) {
-    if(fabs((*a)[i][j]) > fabs(max)) {
-      max = (*a)[i][j];
+    if(fabs(a[i][j]) > fabs(max)) {
+      max = a[i][j];
       top_row = i;
     }
   }
   if(cur_row != top_row) {
     tmp_b = b[j];
-    tmp_row = (*a)[j];
-    (*a)[j] = (*a)[top_row];
-    (*a)[top_row] = tmp_row;
+    tmp_row = a[j];
+    a[j] = a[top_row];
+    a[top_row] = tmp_row;
     b[j] = b[top_row];
     b[top_row] = tmp_b;
   }
@@ -148,7 +148,7 @@ void pivot(double*** a, double* b, int j, int n) {
  * T is the ratio between to rows first index.
  * The calls to gauss_row are split up by row via the k variable.
  */
-void gauss(double*** a, double* b, int i, int n, int thread_count) {
+void gauss(double** a, double* b, int i, int n, int thread_count) {
   double t;
   int start = i;
   int k;
@@ -156,7 +156,7 @@ void gauss(double*** a, double* b, int i, int n, int thread_count) {
   default(none) shared(a, b, t, i, n, start, thread_count) \
   private(k)
   for(k = i+1; k < n; k++) {
-    t = ((*a)[k][i]) / ((*a)[i][i]);
+    t = (a[k][i]) / (a[i][i]);
     gauss_row(a, b, k, i, n, t, start, thread_count);
   }
 }
@@ -168,10 +168,10 @@ void gauss(double*** a, double* b, int i, int n, int thread_count) {
  * Each column of a and the corresponding index of b are adjusted
  * according to the algorithm.
  */
-void gauss_row(double*** a, double* b, int i, int j, int n, double t, int start
+void gauss_row(double** a, double* b, int i, int j, int n, double t, int start
     , int thread_count) {
   for(j=j; j < n; j++) {
-    (*a)[i][j] = (*a)[i][j] - ((*a)[start][j])*t;    
+    a[i][j] = a[i][j] - (a[start][j])*t;    
   }
     (b)[i] = (b)[i] - (b)[start]*t;
 }
@@ -182,11 +182,11 @@ void gauss_row(double*** a, double* b, int i, int j, int n, double t, int start
  * THen the next X up the line is solved and b is again adjusted.
  * Repeat until all X are found.
  */
-void back_sub(double*** a, double** b, double** x, int n) {
+void back_sub(double** a, double* b, double* x, int n) {
   for(int j = (n-1); j >= 0; j--) {
-    (*x)[j] = (*b)[j] / (*a)[j][j];
+    x[j] = b[j] / a[j][j];
     for(int i = (n-1); i >=0; i--) {
-      (*b)[i] -= (*a)[i][j] * (*x)[j];
+      b[i] -= a[i][j] * x[j];
     }
   }
 }
@@ -197,19 +197,19 @@ void back_sub(double*** a, double** b, double** x, int n) {
  * Take the square root of the sum of the square of the differences
  * of each index of the original b and this solved for b.
  */
-double solve_iS(double*** M, double** B, double** x, int n) {
+double solve_iS(double** M, double* B, double* x, int n) {
   double* b;
   double iS = 0;
   b = calloc(n, sizeof(double));
 
   for(int i = 0; i < n; i++) {
     for(int j = 0; j < n; j++) {
-      b[i] += (*M)[i][j] * (*x)[j];
+      b[i] += M[i][j] * x[j];
     }
   }
   
   for(int i = 0; i < n; i++) {
-    iS += pow((*B)[i] - b[i], 2);
+    iS += pow(B[i] - b[i], 2);
   }
 
   return sqrt(iS);
